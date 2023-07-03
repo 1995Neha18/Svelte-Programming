c8887fdde684912d88e38788eac0d3bb1709c3f5
unknown <1802.nehasahu@gmail.com>
2023-07-03 11:35:50 +0530

    routing

--- svelte-project/src/routes/+page.svelte
+++ svelte-project/src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to Home</h1>
+<script>
+  import { goto } from "$app/navigation";
+
+  const handleClick = () => {
+    console.log("Placing your order");
+    goto("/products")
+  }
+</script>
+
+<h1>Welcome to Home!</h1>
+
+<a href="/about">About</a>
+<a href="/products">Product Page</a>
+<button on:click={handleClick}>Place Order</button>
 
--- svelte-project/src/routes/products/+page.svelte
+++ svelte-project/src/routes/products/+page.svelte
@@ -1,1 +1,6 @@
 <h1>Product List</h1>
+
+<h2>Product List 1</h2>
+<h2>Product List 2</h2>
+<h2>Product List 3</h2>
+<h2>Product List 4</h2>
